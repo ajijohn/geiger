0bb062ae78a94c0c156dec1663281a79fad78937
Kevin Xu <khxu@berkeley.edu>
2018-06-09 10:11:21 -0700

    add initial anemometer class files

---
+++ anemometer/Anemometer.h
@@ -0,0 +1,33 @@
+/*
+  This is the header file (interface file) for the Anemometer class. 
+
+  The Anemometer class can be used to work with multiple anemometers more easily.
+*/
+
+#ifndef Anemometer_h
+#define Anemometer_h
+
+#include "Arduino.h"
+
+class Anemometer {
+  private:
+    int _pin;
+    int _distancePerPulse;
+    int _pulses;
+    int _interval;
+    int _lastRecordedPulses;
+    unsigned long _intervalTime;
+    unsigned long _debounceDuration;
+    unsigned long _lastDebounceTime;
+    int _prevState;
+  public:
+    Anemometer(int pin, int distancePerPulse = 1, int interval = 3000, int debounceDuration = 10);
+    void setup();
+    void update();
+    float getWindSpeedMPH();
+    float getWindSpeedKMPH();
+    void setInterval(int interval);
+    void setDistancePerPulse(int distance);
+};
+
+#endif
